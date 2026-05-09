2e8867ea906772518eb957fea885bf6324aa35e7
Click Studios Digital <ioanclickmihalca@gmail.com>
2026-05-09 15:52:14 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 cd ~/domains/snow-n-stuff.com/public_html
 
 git pull origin main
-/opt/alt/php83/usr/bin/php composer.phar install --no-dev --optimize-autoloader
-/opt/alt/php83/usr/bin/php artisan migrate --force
-/opt/alt/php83/usr/bin/php artisan config:cache
-/opt/alt/php83/usr/bin/php artisan route:cache
-/opt/alt/php83/usr/bin/php artisan view:cache
+/opt/alt/php84/usr/bin/php composer.phar install --no-dev --optimize-autoloader
+/opt/alt/php84/usr/bin/php artisan migrate --force
+/opt/alt/php84/usr/bin/php artisan config:cache
+/opt/alt/php84/usr/bin/php artisan route:cache
+/opt/alt/php84/usr/bin/php artisan view:cache
